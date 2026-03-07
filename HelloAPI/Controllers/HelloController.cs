f4eb5a0e19183fc0ffb8e1b5263dfa5745afac09
seemahashimthirdeye <seemahashim.3rdeye@gmail.com>
2026-03-08 00:56:23 +0530

    initial Commit-changed in controller

--- HelloAPI/Controllers/HelloController.cs
+++ HelloAPI/Controllers/HelloController.cs
@@ -9,7 +9,7 @@ namespace HelloAPI.Controllers
         [HttpGet]
         public IActionResult GetMessage()
         {
-            return Ok("Hello From ASP.NET CORE Practice API");
+            return Ok("Hello From ASP.NET CORE Practice API I have changed many things");
         }
 
     }
