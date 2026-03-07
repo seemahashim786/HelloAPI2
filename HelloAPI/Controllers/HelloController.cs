efb6930d19acbe48359fcd1cc2c3c4bf6e9ee1d7
seemahashimthirdeye <seemahashim.3rdeye@gmail.com>
2026-03-08 00:24:26 +0530

    Add project files.

---
+++ HelloAPI/Controllers/HelloController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HelloAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HelloController : Controller
+    {
+        [HttpGet]
+        public IActionResult GetMessage()
+        {
+            return Ok("Hello From ASP.NET CORE Practice API");
+        }
+
+    }
+}
